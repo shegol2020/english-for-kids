e97d8cc520603ca21b9229de013634806753aad3
shegol2020 <shegol2007@gmail.com>
2023-05-25 18:14:35 +0300

    feat: installed webpack and dependencies

---
+++ src/main-page.js
@@ -0,0 +1,4 @@
+import {ANSWER} from "./common";
+import './css/styles.css'
+
+console.log(ANSWER, ANSWER, ANSWER);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/main-page.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    mode: 'development'
+}
