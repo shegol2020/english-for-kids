b50e1d2a55995ccecd6bf4ba3454a1e9e3a15230
shegol2020 <shegol2007@gmail.com>
2023-08-27 15:55:32 +0300

    feat: sidebar button

--- src/common.js
+++ src/common.js
@@ -1,5 +1,26 @@
 import "./css/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+/* sidebar */
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.querySelector(".sidebar-container");
+const srMenu = document.querySelector(".sr-menu")
+
+document.addEventListener("DOMContentLoaded", () =>{
+    console.log(sidebar.classList)
+})
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+
+    if(sidebar.classList.contains("show")){
+        srMenu.innerText = "Close menu";
+        menuBtn.setAttribute("aria-expanded", "true")
+    } else {
+        srMenu.innerText = "Open menu";
+        menuBtn.setAttribute("aria-expanded", "false")
+    }
+});
 
 
